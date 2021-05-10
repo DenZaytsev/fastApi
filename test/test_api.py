1b673eacd7de9a1e96010ad498a6db5a42172e8f
denis <d.zaicev@dm-solutions.ru>
2021-05-11 00:01:27 +0300

    test ci

--- test/test_api.py
+++ test/test_api.py
@@ -9,4 +9,8 @@ client = TestClient(fast_app)
 def test_first_view():
     response = client.get('/')
     assert response.status_code == 200
-    assert response.json() == {"message":"TeSt"}
+    assert response.json() == {"message": "TeSt"}
+
+
+def test_invalid_test():
+    assert False
